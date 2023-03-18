17a6194b7e79d43ae6d386f6a711658b3eeb6319
Răzvan Trufin <117245410+razvantrufin99x@users.noreply.github.com>
2023-03-18 23:16:39 +0200

    Add files via upload
    DO NOT FORGET THIS

---
+++ grups.cs
@@ -0,0 +1,117 @@
+
+public List<int> grupsusc2c3 = new List<int>();
+public List<int> grupmijlocstangac3c1 = new List<int>();
+public List<int> grupmijlocdreaptac1c2 = new List<int>();
+public List<int> grupjostsangac1c2 = new List<int>();
+public List<int> grupjosmijlocc2c2 = new List<int>();
+public List<int> grupjosdreaptac1c3 = new List<int>();
+
+public void addInsidegrupsusc2c3()
+{
+	
+	grupsusc2c3.Add(39);
+	grupsusc2c3.Add(40);
+	grupsusc2c3.Add(41);
+	
+	grupsusc2c3.Add(45);
+	grupsusc2c3.Add(46);
+	grupsusc2c3.Add(47);
+	
+	grupsusc2c3.Add(51);
+	grupsusc2c3.Add(52);
+	grupsusc2c3.Add(53);
+	
+}
+
+public void grupmijlocstangac3c1()
+{
+	
+	grupsusc2c3.Add(3);
+	grupsusc2c3.Add(4);
+	grupsusc2c3.Add(5);
+	
+	grupsusc2c3.Add(15);
+	grupsusc2c3.Add(16);
+	grupsusc2c3.Add(17);
+	
+	grupsusc2c3.Add(27);
+	grupsusc2c3.Add(28);
+	grupsusc2c3.Add(29);
+	
+}
+
+
+public void grupmijlocdreaptac1c2()
+{
+	
+	grupsusc2c3.Add(6);
+	grupsusc2c3.Add(7);
+	grupsusc2c3.Add(8);
+	
+	grupsusc2c3.Add(18);
+	grupsusc2c3.Add(19);
+	grupsusc2c3.Add(20);
+	
+	grupsusc2c3.Add(30);
+	grupsusc2c3.Add(32);
+	grupsusc2c3.Add(32);
+	
+}
+
+
+public void grupjostsangac1c2()
+{
+	
+	grupsusc2c3.Add(0);
+	grupsusc2c3.Add(1);
+	grupsusc2c3.Add(2);
+	
+	grupsusc2c3.Add(12);
+	grupsusc2c3.Add(13);
+	grupsusc2c3.Add(14);
+	
+	grupsusc2c3.Add(24);
+	grupsusc2c3.Add(25);
+	grupsusc2c3.Add(26);
+	
+}
+
+
+public void grupjosmijlocc2c2()
+{
+	
+	grupsusc2c3.Add(36);
+	grupsusc2c3.Add(37);
+	grupsusc2c3.Add(38);
+	
+	grupsusc2c3.Add(42);
+	grupsusc2c3.Add(43);
+	grupsusc2c3.Add(44);
+	
+	grupsusc2c3.Add(48);
+	grupsusc2c3.Add(49);
+	grupsusc2c3.Add(50);
+	
+}
+
+
+public void grupjosdreaptac1c3()
+{
+	
+	grupsusc2c3.Add(9);
+	grupsusc2c3.Add(10);
+	grupsusc2c3.Add(1);
+	
+	grupsusc2c3.Add(21);
+	grupsusc2c3.Add(22);
+	grupsusc2c3.Add(23);
+	
+	grupsusc2c3.Add(33);
+	grupsusc2c3.Add(34);
+	grupsusc2c3.Add(35);
+	
+}
+
+
+
+
